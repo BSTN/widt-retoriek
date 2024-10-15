82dc0c9a341ac4cd87fffa5408ec6da7e2af6ad0
BSTN <basten@bstn.nl>
2024-10-15 14:37:21 +0100

    update random not matching subjectief/objectief + nr

--- stores/main.store.ts
+++ stores/main.store.ts
@@ -61,12 +61,28 @@ export const useMainStore = defineStore({
       const reacties3shuffled = shuffle(reacties3)
       const reacties4shuffled = shuffle(reacties4)
       const chunks1 = chunk(reacties1shuffled, 4);
-      const chunks2 = chunk(reacties2shuffled, 4);
+      // const chunks2 = chunk(reacties2shuffled, 4);
+      const chunks2 = []
+      for (let i in reacties2shuffled) {
+        if (!chunks1[0].find(x => x.reference.replace('objectief','') === reacties2shuffled[i].reference.replace('subjectief', ''))) {
+          chunks2.push(reacties2shuffled[i])
+        }
+        if (chunks2.length === 4) break;
+      }
+
       const chunks3 = chunk(reacties3shuffled, 4);
-      const chunks4 = chunk(reacties4shuffled, 4);
+      // const chunks4 = chunk(reacties4shuffled, 4);
+      const chunks4 = []
+      for (let i in reacties4shuffled) {
+        if (!chunks3[0].find(x => x.reference.replace('objectief','') === reacties4shuffled[i].reference.replace('subjectief', ''))) {
+          chunks4.push(reacties4shuffled[i])
+        }
+        if (chunks4.length === 4) break;
+      }
+
       if (random == 1 || random == 2) {
-        this.answers._reacties1 = shuffle(chunks1[0].map(x => x.reference).concat(chunks2[0].map(x => x.reference)))
-        this.answers._reacties2 = shuffle(chunks3[0].map(x => x.reference).concat(chunks4[0].map(x => x.reference)))
+        this.answers._reacties1 = shuffle(chunks1[0].map(x => x.reference).concat(chunks2.map(x => x.reference)))
+        this.answers._reacties2 = shuffle(chunks3[0].map(x => x.reference).concat(chunks4.map(x => x.reference)))
       }
       if (random == 3 || random == 4) {
         this.answers._reacties1 = shuffle(chunks3[0].map(x => x.reference).concat(chunks4[0].map(x => x.reference)))
